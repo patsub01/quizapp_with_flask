75943e55e2fd50b3b6b2427a2f4b5f9a7b83e953
patsub01 <patelkhana@gmail.com>
2024-08-06 19:09:50 +0530

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,45 @@
+# questions.py
+questions = {
+    'General Knowledge': [
+    {'question': 'Who invented electricity?', 'options': ['Benjamin Franklin', 'Alexander Fleming', 'Isaac Newton', 'Albert Einstein'], 'answer': 'Benjamin Franklin'},
+    {'question': 'Who invented Watch?', 'options': ['Isaac Newton', 'Alexander Fleming', 'Peter Henlein', 'John Stephenson'], 'answer': 'Peter Henlein'},
+    {'question': 'Name the hardest substance available on Earth?', 'options': ['Copper', 'Nickel', 'Aluminium', 'Diamond'], 'answer': 'Diamond'},
+    {'question': 'Who discovered the X-rays first?', 'options': ['Linus Pauling', 'Wilhelm Roentgen', 'Thomas Edison', 'Dmitri Mendeleev'], 'answer': 'Wilhelm Roentgen'},
+    {'question': 'Who Invented the First Computer?', 'options': ['Edward Jenner', 'Issac Newton', 'Charles Babbage', 'Dmitri Mendeleev'], 'answer': 'Charles Babbage'},
+    {'question': 'What art form is described as "decorative handwriting or handwritten lettering"?', 'options': ['Slanting handwriting', 'Bold letterwritingl', 'Calligraphy', 'Capital letterwriting'], 'answer': 'Calligraphy'}, 
+    {'question': 'In what country would you find Mount Kilimanjaro?', 'options': ['Kenya', 'Botswana', 'Nigeria', 'Tanzania'], 'answer': 'Tanzania'},
+    {'question': 'What software company is headquartered in Redmond, Washington?', 'options': ['IBM', 'Microsoft', 'Apple', 'Cisco'], 'answer': 'Microsoft'},
+    {'question': 'Which river is the longest in the world?', 'options': ['Amazon', 'Nile', 'Yangtze', 'Mississippi'], 'answer': 'Nile'},    
+    {'question': 'Which planet has the most moons?', 'options': ['Jupitor', 'Earth', 'Saturn', 'Venus'], 'answer': 'Saturn'},
+        # Add more questions for the General Knowledge category
+    ],
+ #   'Science': [
+ #       {'question': 'What is the chemical symbol for water?', 'options': ['H2O', 'CO2', 'O2', 'N2'], 'answer': 'H2O'},
+#      {'question': 'Who developed the theory of relativity?', 'options': ['Isaac Newton', 'Albert Einstein', 'Galileo Galilei', 'Stephen Hawking'], 'answer': 'Albert Einstein'},
+ #       # Add more questions for the Science category
+ #   ],
+    'Sports': [
+    {'question': 'Who was Player of Tournament in 2024 T20 World Cup?', 'options': ['Rohit Sharma', 'Virat Kohli', 'Jasprit Bumrah', 'Jadeja'], 'answer': 'Jasprit Bumrah'},
+    {'question': 'Who was Man of the match in 2024 T20 World Cup final?', 'options': ['Jasprit Bumrah', 'Arshdeep Singh', 'Virat Kohli', 'Rohit Sharma'], 'answer': 'Virat Kohli'}, 
+    {'question': 'What is the name of T20 cricket tournament played in the USA?', 'options': ['BBLh', 'MLB', 'MLC', 'MLD'], 'answer': 'MLC'},
+    {'question': 'Rohit Sharma is one of two players to have played in all the 9 T20 world cups played so far, who is the other player?', 'options': ['Virat Kohli', 'David Warner', 'Shakib Al Hasan', 'Kane Williamson'], 'answer': 'Shakib Al Hasan'},
+    {'question': 'In test cricket history, only two times test ended in a tie, which team was involved in both matches?', 'options': ['India', 'England', 'Pakistan', 'Australia'], 'answer': 'Australia'},  
+    {'question': 'In which year was the first Cricket World Cup held?', 'options': ['1983', '1974', '1975', '1970'], 'answer': '1975'}, 
+    {'question': 'Which player has the most centuries in Cricket World Cup history?', 'options': ['Ricky Ponting', 'Sachin Tendulkar', 'Rohit Sharma', 'Virat Kohli'], 'answer': 'Rohit Sharma'},    
+    {'question': 'Who won the first IPL in 2008?', 'options': ['Chennai Super Kings', 'Rajasthan Royals', 'Delhi Capitals', 'Luckow Super Giants'], 'answer': 'Rajasthan Royals'}, 
+    {'question': 'Which team won the inagural ICC Champions Trophy held in 1998?', 'options': ['India', 'Pakistan', 'South Africa', 'Australia'], 'answer': 'South Africa'},  
+    {'question': 'First ever test match and centenary test match played between Australia and England ended in the same margin of victory for Australia, what was that margin??', 'options': ['100 runs', '125 runs', '45 runs', '34 runs'], 'answer': '45 runs'},  
+        # Add more questions for the History category
+    ],
+#    'Mathematics': [
+#       {'question': 'What is the value of pi (π) to two decimal places?', 'options': ['3.14', '3.16', '3.18', '3.20'], 'answer': '3.14'},
+#        {'question': 'Solve the equation: 2x + 5 = 11', 'options': ['3', '4', '5', '6'], 'answer': '3'},
+        # Add more questions for the Mathematics category
+#    ],
+#    'Geography': [
+#       {'question': 'Which river is the longest in the world?', 'options': ['Amazon', 'Nile', 'Yangtze', 'Mississippi'], 'answer': 'Nile'},
+#        {'question': 'What is the capital of Australia?', 'options': ['Sydney', 'Melbourne', 'Canberra', 'Brisbane'], 'answer': 'Canberra'},
+        # Add more questions for the Geography category
+#    ], 
+    # Add more categories as needed
+}
